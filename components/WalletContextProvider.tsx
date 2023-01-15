35dfad37eb6c290d08d926a1969586ae62d928b1
Merto <mertenercan@gmail.com>
2023-01-15 07:55:02 +0300

    I HATE NODE MODULES

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -3,12 +3,13 @@ import { FC, ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import * as web3 from '@solana/web3.js'
-import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
+
 import '@solana/wallet-adapter-react-ui/styles.css'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 
 const WalletContextProvider: FC<{ children:any }> = ({ children }) => {
   const endpoint = web3.clusterApiUrl('devnet')
-  const wallets:any = new walletAdapterWallets.PhantomWalletAdapter()
+  const wallets:any = new PhantomWalletAdapter()
 
 	return (
 	<ConnectionProvider endpoint={endpoint}>
